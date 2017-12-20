3f8536ed9de4a9cadec91a20aa03d49766950857
kazuma8128 <kazuma@kmc.gr.jp>
2017-12-20 19:13:10 +0900

    modify dice

--- Others/dice.cpp
+++ Others/dice.cpp
@@ -1,6 +1,9 @@
 
 using pii = pair<int, int>;
 
+// faces are numbered 0:top, 1:front, 2:right, 3:left, 4:back, 5:bottom
+using pii = pair<int, int>;
+
 struct dice {
 	vector<pii> x;
 	dice(int x_[6] = (int[]) { 1, 2, 3, 4, 5, 6 }) : x(3) {
@@ -9,37 +12,61 @@ struct dice {
 		x[2] = pii(x_[2], x_[3]);
 	}
 	dice(const dice& d) : x(d.x) {}
+	// copy dice
 	dice& operator=(dice d) {
 		x = d.x;
 		return *this;
 	}
+	// get value on n-th face
 	int X(int n) {
 		return n < 3 ? x[n].first : x[5 - n].second;
 	}
+	// get value on side face to dir
+	int Xside(int dir) {
+		return dir / 2 ? x[2 - (dir & 1)].second : x[2 - (dir & 1)].first;
+	}
+	// roll to the north
 	void N() {
 		swap(x[0], x[1]);
 		swap(x[1].first, x[1].second);
 	}
+	// roll to the east
 	void E() {
-		swap(x[0], x[2]);
-		swap(x[0].first, x[0].second);
-	}
-	void W() {
 		swap(x[0], x[2]);
 		swap(x[2].first, x[2].second);
 	}
+	// roll to the west
+	void W() {
+		swap(x[0], x[2]);
+		swap(x[0].first, x[0].second);
+	}
+	// roll to the south
 	void S() {
 		swap(x[0], x[1]);
 		swap(x[0].first, x[0].second);
 	}
+	// roll
+	void roll(int dir) {
+		switch (dir)
+		{
+		case 0: W(); break;
+		case 1: S(); break;
+		case 2: E(); break;
+		case 3: N(); break;
+		default: break;
+		}
+	}
+	// roll clockwise
 	void C() {
 		swap(x[1], x[2]);
 		swap(x[2].first, x[2].second);
 	}
+	// roll counterclockwise
 	void RC() {
 		swap(x[1], x[2]);
 		swap(x[1].first, x[1].second);
 	}
+	// roll so that val is on the top
 	void top(int val) {
 		if (x[0].first == val) {
 			return;
@@ -48,10 +75,10 @@ struct dice {
 			N();
 		}
 		else if (x[2].first == val) {
-			W();
+			E();
 		}
 		else if (x[2].second == val) {
-			E();
+			W();
 		}
 		else if (x[1].second == val) {
 			S();
@@ -60,6 +87,7 @@ struct dice {
 			N(); N();
 		}
 	}
+	// roll clockwise or counterclockwise so that val is on the front
 	void front(int val) {
 		if (x[1].first == val) {
 			return;
@@ -75,32 +103,6 @@ struct dice {
 			swap(x[2].first, x[2].second);
 		}
 	}
-	set<dice> all() {
-		set<dice> res;
-		dice d(*this);
-		dice tmp(d);
-		for (int i = 0; i < 4; i++) {
-			tmp = d;
-			for (int j = 0; j < 4; j++) {
-				res.insert(tmp);
-				tmp.C();
-			}
-			d.N();
-		}
-		d.W();
-		tmp = d;
-		for (int i = 0; i < 4; i++) {
-			res.insert(tmp);
-			tmp.C();
-		}
-		d.E(); d.E();
-		tmp = d;
-		for (int i = 0; i < 4; i++) {
-			res.insert(tmp);
-			tmp.C();
-		}
-		return res;
-	}
 };
 
 bool operator==(const dice& l, const dice& r) {
@@ -110,3 +112,6 @@ bool operator==(const dice& l, const dice& r) {
 bool operator<(const dice& l, const dice& r) {
 	return l.x < r.x;
 }
+
+const int dx[] = { 0, 1, 0, -1 };
+const int dy[] = { 1, 0, -1, 0 };
