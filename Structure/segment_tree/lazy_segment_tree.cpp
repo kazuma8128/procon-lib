68c5aca196d28b362b4a21162daeed49642d1728
kazuma8128 <kazuma@kmc.gr.jp>
2017-12-26 03:34:09 +0900

    modify lazy segment tree and layerd range trree

--- Structure/segment_tree/lazy_segment_tree.cpp
+++ Structure/segment_tree/lazy_segment_tree.cpp
@@ -37,65 +37,101 @@ public:
 	}
 };
 
-// lazy segment tree
-struct RARMQ {
-	using type = int;
-	static type id1() { return INT_MAX; }
-	static type id2() { return 0; }
-	static type op1(const type& l, const type & r) { return min(l, r); }
-	static type op2(const type& l, const type & r) { return l + r; }
+// monoids
+struct RU {
+	using t1 = int;
+	using t2 = int;
+	static t1 id1() { return INT_MAX; }
+	static t2 id2() { return -1; }
+	static t1 op1(const t1& l, const t1& r) { return min(l, r); }
+	static t1 op2(const t1& l, const t2& r) { return r == id2() ? l : r; }
+	static t2 op3(const t2& l, const t2& r) { return r == id2() ? l : r; }
+};
+
+struct RA {
+	using t1 = int;
+	using t2 = int;
+	static t1 id1() { return 0; }
+	static t2 id2() { return 0; }
+	static t1 op1(const t1& l, const t1& r) { return l + r; }
+	static t1 op2(const t1& l, const t2& r) { return l + r; }
+	static t2 op3(const t2& l, const t2& r) { return l + r; }
+};
+
+struct RURM {
+	using t1 = int;
+	using t2 = int;
+	static t1 id1() { return INT_MAX; }
+	static t2 id2() { return -1; }
+	static t1 op1(const t1& l, const t1& r) { return min(l, r); }
+	static t1 op2(const t1& l, const t2& r) { return r != id2() ? r : l; }
+	static t2 op3(const t2& l, const t2& r) { return r != id2() ? r : l; }
 };
 
 struct RURS {
-	using type = int;
-	static type id1() { return 0; }
-	static type id2() { return -1; }
-	static type op1(const type& l, const type & r) { return l + r; }
-	static type op2(const type& l, const type & r) { return r != -1 ? r : l; }
+	using ll = long long;
+	struct t1 {
+		ll val, size;
+		t1(ll v, ll s = 1) : val(v), size(s) {}
+	};
+	using t2 = ll;
+	static t1 id1() { return t1(0, 0); }
+	static t2 id2() { return -1; }
+	static t1 op1(const t1& l, const t1& r) { return t1(l.val + r.val, l.size + r.size); }
+	static t1 op2(const t1& l, const t2& r) { return t1(l.size * r, l.size); }
+	static t2 op3(const t2& l, const t2& r) { return r != id2() ? r : l; }
 };
 
-struct RURMQ {
-	using type = int;
-	static type id1() { return INT_MAX; }
-	static type id2() { return -1; }
-	static type op1(const type& l, const type & r) { return min(l, r); }
-	static type op2(const type& l, const type & r) { return r != -1 ? r : l; }
+struct RARS {
+	using ll = long long;
+	struct t1 {
+		ll val, size;
+		t1(ll v, ll s = 1) : val(v), size(s) {}
+	};
+	using t2 = ll;
+	static t1 id1() { return t1(0, 0); }
+	static t2 id2() { return 0; }
+	static t1 op1(const t1& l, const t1& r) { return t1(l.val + r.val, l.size + r.size); }
+	static t1 op2(const t1& l, const t2& r) { return t1(l.val + l.size * r, l.size); }
+	static t2 op3(const t2& l, const t2& r) { return l + r; }
 };
 
 struct RURMS {
+	using ll = long long;
 	static const ll INF = 1e17;
-	struct type {
+	struct t1 {
 		ll left, right, all, sum, size;
-		type() : left(-INF), right(-INF), all(-INF), sum(0), size(0) {}
-		type(ll val) : left(val), right(val), all(val), sum(val), size(1) {}
-		type(ll l, ll r, ll a, ll s, ll si) : left(l), right(r), all(a), sum(s), size(si) {}
-		bool operator==(const type& r) const {
+		t1() : left(-INF), right(-INF), all(-INF), sum(0), size(0) {}
+		t1(ll val) : left(val), right(val), all(val), sum(val), size(1) {}
+		t1(ll l, ll r, ll a, ll s, ll si) : left(l), right(r), all(a), sum(s), size(si) {}
+		bool operator==(const t1& r) const {
 			return left == r.left && right == r.right && all == r.all && sum == r.sum && size == r.size;
 		}
 	};
-	static type id1() { return type(); }
-	static type id2() { return type(-INF, -INF, -INF, -INF, -1); }
-	static type op1(const type& l, const type & r) {
-		return type(max(l.left, l.sum + r.left), max(r.right, r.sum + l.right), max({ l.all, r.all, l.right + r.left }), l.sum + r.sum, l.size + r.size);
+	using t2 = ll;
+	static t1 id1() { return t1(); }
+	static t2 id2() { return -INF; }
+	static t1 op1(const t1& l, const t1 & r) {
+		return t1(max(l.left, l.sum + r.left), max(r.right, r.sum + l.right), max({ l.all, r.all, l.right + r.left }), l.sum + r.sum, l.size + r.size);
 	}
-	static type op2(const type& l, const type & r) {
+	static t1 op2(const t1& l, const t2 & r) {
 		if (r == id2()) return l;
-		if (l.size < 0) return r;
-		ll val = r.all;
-		if (val > 0) {
-			return type(val * l.size, val * l.size, val * l.size, val * l.size, l.size);
-		}
-		else {
-			return type(val, val, val, val * l.size, l.size);
-		}
+		if (r > 0) return t1(r * l.size, r * l.size, r * l.size, r * l.size, l.size);
+		return t1(r, r, r, r * l.size, l.size);
+	}
+	static t2 op3(const t2& l, const t2 & r) {
+		return r == id2() ? l : r;
 	}
 };
 
+// lazy segment tree
 template <typename M>
-class lSegmentTree {
-	using T = typename M::type;
+class LazySegmentTree {
+	using T1 = typename M::t1;
+	using T2 = typename M::t2;
 	const int n;
-	vector<T> data, lazy;
+	vector<T1> data;
+	vector<T2> lazy;
 	int size(int n) {
 		int res = 1;
 		while (res < n) res <<= 1;
@@ -104,46 +140,48 @@ class lSegmentTree {
 	void push(int node) {
 		if (lazy[node] == M::id2()) return;
 		if (node < n) {
-			lazy[node * 2] = M::op2(lazy[node * 2], lazy[node]);
-			lazy[node * 2 + 1] = M::op2(lazy[node * 2 + 1], lazy[node]);
+			lazy[node * 2] = M::op3(lazy[node * 2], lazy[node]);
+			lazy[node * 2 + 1] = M::op3(lazy[node * 2 + 1], lazy[node]);
 		}
 		data[node] = M::op2(data[node], lazy[node]);
 		lazy[node] = M::id2();
 	}
-	void suc(int l, int r, int node, int lb, int ub, T val) {
+	void suc(int l, int r, int node, int lb, int ub, T2 val) {
 		if (ub <= l || r <= lb) return;
-		push(node);
 		if (l <= lb && ub <= r) {
-			lazy[node] = val;
+			lazy[node] = M::op3(lazy[node], val);
 			return;
 		}
+		push(node);
 		int c = (lb + ub) / 2;
 		suc(l, r, node * 2, lb, c, val);
 		suc(l, r, node * 2 + 1, c, ub, val);
 		data[node] = M::op1(M::op2(data[node * 2], lazy[node * 2]), M::op2(data[node * 2 + 1], lazy[node * 2 + 1]));
 	}
-	T sub(int l, int r, int node, int lb, int ub) {
+	T1 sub(int l, int r, int node, int lb, int ub) {
 		if (ub <= l || r <= lb) return M::id1();
+		if (l <= lb && ub <= r) return M::op2(data[node], lazy[node]);
 		push(node);
-		if (l <= lb && ub <= r) {
-			return data[node];
-		}
 		int c = (lb + ub) / 2;
 		return M::op1(sub(l, r, node * 2, lb, c), sub(l, r, node * 2 + 1, c, ub));
 	}
 public:
-	lSegmentTree(int n_) : n(size(n_)), data(n * 2, M::id1()), lazy(n * 2, M::id2()) {}
-	void init(const vector<T>& data_) {
-		for (int i = 0; i < (int)data_.size(); i++)
-			data[i + n] = data_[i];
-		for (int i = n - 1; i >= 0; i--)
-			data[i] = M::op1(data[i * 2], data[i * 2 + 1]);
-		fill(lazy.begin(), lazy.end(), M::id2());
+	LazySegmentTree(int n_) : n(size(n_)), data(n * 2, M::id1()), lazy(n * 2, M::id2()) {}
+	LazySegmentTree(int n_, T1 v1, T2 v2) : n(size(n_)), data(n * 2, v1), lazy(n * 2, v2) {}
+	LazySegmentTree(const vector<T1>& data_) : n(size(data_.size())), data(n * 2, M::id1()), lazy(n * 2, M::id2()) {
+		init(data_);
 	}
-	void update(int l, int r, T val) {
+	void init() {
+		for (int i = n - 1; i >= 1; i--) data[i] = M::op1(data[i * 2], data[i * 2 + 1]);
+	}
+	void init(const vector<T1>& data_) {
+		for (int i = 0; i < (int)data_.size(); i++) data[i + n] = data_[i];
+		init();
+	}
+	void update(int l, int r, T2 val) {
 		suc(l, r + 1, 1, 0, n, val);
 	}
-	T find(int l, int r) {
+	T1 find(int l, int r) {
 		return sub(l, r + 1, 1, 0, n);
 	}
 };
--- Structure/segment_tree/range_tree/layerd_range_tree.cpp
+++ Structure/segment_tree/range_tree/layerd_range_tree.cpp
@@ -4,7 +4,7 @@ template <typename T>
 class LayerdRangeTree {
 	const int n;
 	vector<vector<T>> data;
-	vector<vector<int>> tol, tor;
+	vector<vector<pair<int, int>>> to;
 	int size(int n) {
 		int res;
 		for (res = 1; res < n; res <<= 1);
@@ -13,14 +13,14 @@ class LayerdRangeTree {
 	int sub(int l, int r, int left, int right, int node, int lb, int ub) {
 		if (ub <= l || r <= lb) return 0;
 		if (l <= lb && ub <= r) return right - left;
-		return sub(l, r, tol[node][left], tol[node][right], node * 2, lb, (lb + ub) / 2)
-			+ sub(l, r, tor[node][left], tor[node][right], node * 2 + 1, (lb + ub) / 2, ub);
+		return sub(l, r, to[node][left].first, to[node][right].first, node * 2, lb, (lb + ub) / 2)
+			+ sub(l, r, to[node][left].second, to[node][right].second, node * 2 + 1, (lb + ub) / 2, ub);
 	}
 	void merge(int node) {
 		int ln = node * 2, rn = node * 2 + 1;
 		int ll = data[ln].size(), rl = data[rn].size();
 		for (int i = 0, j = 0; i < ll || j < rl;) {
-			tol[node].push_back(i), tor[node].push_back(j);
+			to[node].emplace_back(i, j);
 			if (i == ll) {
 				data[node].push_back(data[rn][j++]);
 			}
@@ -34,17 +34,16 @@ class LayerdRangeTree {
 				data[node].push_back(data[rn][j++]);
 			}
 		}
-		tol[node].push_back(ll), tor[node].push_back(rl);
+		to[node].emplace_back(ll, rl);
 	}
 public:
-	LayerdRangeTree(const vector<T>& data_) : n(size(data_.size())), data(n * 2), tol(n), tor(n) {
+	LayerdRangeTree(const vector<T>& data_) : n(size(data_.size())), data(n * 2), to(n) {
 		for (int i = 0; i < (int)data_.size(); i++)
 			data[i + n].push_back(data_[i]);
-		for (int i = n - 1; i >= 0; i--) {
+		for (int i = n - 1; i >= 1; i--) {
 			int ns = data[i * 2].size() + data[i * 2 + 1].size();
 			data[i].reserve(ns);
-			tol[i].reserve(ns + 1);
-			tor[i].reserve(ns + 1);
+			to[i].reserve(ns + 1);
 			merge(i);
 		}
 	}
