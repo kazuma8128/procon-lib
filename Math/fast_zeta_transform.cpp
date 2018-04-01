f831832a7ef38b727724a54700b3ecd6ebaf63bd
kazuma8128 <kazuma@kmc.gr.jp>
2018-04-01 21:00:29 +0900

    modify file name

---
+++ Math/fast_zeta_transform.cpp
@@ -0,0 +1,12 @@
+
+template <typename T>
+void fzt(vector<T>& f){
+    int n = f.size();
+    for(int i = 0;(1 << i) < n;i++){
+        for(int j = 0;j < n;j++){
+            if(j & (1 << i)){
+                f[j] += f[j ^ (1 << i)];
+            }
+        }
+    }
+}
