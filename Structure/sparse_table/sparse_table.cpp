f20d5ea3fecfe7c178c1548ec01d13d6a3b9c8b3
kazuma8128 <kazuma@kmc.gr.jp>
2018-07-05 08:44:10 +0900

    update sparse table

--- Structure/sparse_table/sparse_table.cpp
+++ Structure/sparse_table/sparse_table.cpp
@@ -1,30 +1,25 @@
 
 template <typename T>
 class sparse_table {
-	int n;
+	const int n;
 	vector<vector<T>> t;
-	T merge(const T& l, const T& r) {
+	T merge(const T& l, const T& r) const {
 		return min(l, r);
 	}
 public:
-	sparse_table(const vector<T>& b) {
-		n = b.size();
-		t.push_back(b);
-		for (int i = 2, j = 1; i <= n; i <<= 1, j++) {
-			t.push_back(vector<T>());
-			for (int k = 0; k + i <= n; k++) {
-				t[j].push_back(merge(t[j - 1][k], t[j - 1][k + i / 2]));
-			}
+	sparse_table(const vector<T>& b) : n(b.size()), t(1, b) {
+		for (int i = 2, j = 1, p = 0; i <= n; i <<= 1, j++, p = 0) {
+			t.emplace_back(n);
+			for (int k = 0; k + i <= n; k++)
+				t[j][p++] = merge(t[j - 1][k], t[j - 1][k + (i >> 1)]);
 		}
 	}
-	T find(int l, int r) {
-		r++;
+	T find(int l, int r) const { // [l, r)
 		assert(0 <= l && l < r && r <= n);
-		unsigned int w = r - l;
-		int i = 31 - __builtin_clz(w);
+		int i = 31 - __builtin_clz(r - l);
 		return merge(t[i][l], t[i][r - (1 << i)]);
 	}
-	T operator[](int id) {
+	T operator[](int id) const {
 		return t[0][id];
 	}
 };
