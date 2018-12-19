4c4337be26f6944b19aac2d4fc5ca80eb312a92d
kazuma8128 <kazuma@kmc.gr.jp>
2018-12-19 21:24:36 +0900

    update Aho-Corasick

--- String/aho-corasick.cpp
+++ String/aho-corasick.cpp
@@ -1,78 +1,92 @@
 
 const int var = 26;	// the number of charactor types
+
 int trans(char c) {
 	return c - 'a';
 }
 
-struct node {
-	node *fail;
-	vector<node*> next;
-	vector<int> ok;
-	node() : fail(nullptr), next(var, nullptr) {}
-};
-
-node* getnext(node* p, char c) {
-	while (p->next[trans(c)] == nullptr) p = p->fail;
-	return p->next[trans(c)];
-}
-
 class aho_corasick {
+	struct ac_node {
+		int fail;
+		vector<int> next;
+		vector<int> ok;
+		ac_node() : fail(-1), next(var, -1) {}
+	};
 	vector<int> unite(const vector<int>& a, const vector<int>& b) {
 		vector<int> res;
 		set_union(a.begin(), a.end(), b.begin(), b.end(), back_inserter(res));
-		return res;
+		return move(res);
 	}
-	int K;
-	node *root;
+	int n;
+	vector<ac_node> dat;
 public:
-	aho_corasick(const vector<string>& Ts) : K(Ts.size()), root(new node) {
-		node *now;
-		root->fail = root;
-		for (int i = 0; i < K; i++) {
+	aho_corasick(const vector<string>& Ts) : n(Ts.size()), dat(1) {
+		int now;
+		dat[0].fail = 0;
+		for (int i = 0; i < n; i++) {
 			auto &T = Ts[i];
-			now = root;
+			now = 0;
 			for (auto c : T) {
-				if (now->next[trans(c)] == nullptr) {
-					now->next[trans(c)] = new node;
+				if (dat[now].next[trans(c)] == -1) {
+					dat[now].next[trans(c)] = dat.size();
+					dat.emplace_back();
 				}
-				now = now->next[trans(c)];
+				now = dat[now].next[trans(c)];
 			}
-			now->ok.push_back(i);
+			dat[now].ok.push_back(i);
 		}
-		queue<node*> q;
+		queue<int> q;
 		for (int i = 0; i < var; i++) {
-			if (root->next[i] == nullptr) {
-				root->next[i] = root;
+			if (dat[0].next[i] == -1) {
+				dat[0].next[i] = 0;
 			}
 			else {
-				root->next[i]->fail = root;
-				q.push(root->next[i]);
+				dat[dat[0].next[i]].fail = 0;
+				q.push(dat[0].next[i]);
 			}
 		}
 		while (!q.empty()) {
 			now = q.front(); q.pop();
 			for (int i = 0; i < var; i++) {
-				if (now->next[i] != nullptr) {
-					node *nx = now->fail;
-					while (nx->next[i] == nullptr) {
-						nx = nx->fail;
+				if (dat[now].next[i] != -1) {
+					int nx = dat[now].fail;
+					while (dat[nx].next[i] == -1) {
+						nx = dat[nx].fail;
 					}
-					now->next[i]->fail = nx->next[i];
-					now->next[i]->ok = unite(now->next[i]->ok, nx->next[i]->ok);
-					q.push(now->next[i]);
+					int nex = dat[now].next[i];
+					dat[nex].fail = dat[nx].next[i];
+					dat[nex].ok = unite(dat[nex].ok, dat[dat[nx].next[i]].ok);
+					q.push(nex);
 				}
 			}
 		}
 	}
-	node* getroot() const {
-		return root;
+	int size() const {
+		return dat.size();
+	}
+	bool is_ok(int id) const {
+		return !dat[id].ok.empty();
+	}
+	int get_next(int id, char c) const {
+		while (dat[id].next[trans(c)] == -1) id = dat[id].fail;
+		return dat[id].next[trans(c)];
 	}
 	vector<int> count(const string& S) const {
-		vector<int> res(K);
-		node *now = root;
+		vector<int> res(n);
+		int now = 0;
 		for (auto c : S) {
-			now = getnext(now, c);
-			for (auto k : now->ok) res[k]++;
+			now = get_next(now, c);
+			for (auto k : dat[now].ok) res[k]++;
+		}
+		return res;
+	}
+	vector<vector<int>> all_count(const string& S) const {
+		int N = S.size();
+		vector<vector<int>> res(N);
+		int now = 0;
+		for (int i = 0; i < N; i++) {
+			now = get_next(now, S[i]);
+			res[i] = dat[now].ok;
 		}
 		return res;
 	}
